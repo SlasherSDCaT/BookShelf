ce3176bcbeb20f49abc6e12d21b094be88de3984
SlasherSDCaT <slashersdcat@gmail.com>
2024-05-20 20:06:42 +0300

    dlskjhdsfkj

--- backend/internal/handler/middleware.go
+++ backend/internal/handler/middleware.go
@@ -47,7 +47,7 @@ func (h *Handler) UserIdentify(next http.Handler) http.Handler {
 
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://bookshelf-ekzd.onrender.com")
+		w.Header().Set("Access-Control-Allow-Origin", "https://bookshelf-1-ph0o.onrender.com")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == http.MethodOptions {
